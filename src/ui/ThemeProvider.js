d59fd558a84c9a45ddd67b36934861a547fe00b2
Mitchell Stewart <mitchell.stewart@mylsb.email>
2019-08-14 10:32:05 +1000

    :lipstick: Add themes, ThemeProvider

---
+++ src/ui/ThemeProvider.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import css from '@styled-system/css'
+import {
+  ThemeProvider as StyledComponentsThemeProvider,
+  createGlobalStyle
+} from 'styled-components'
+
+const GlobalStyles = () => {
+  const GlobalStyle = createGlobalStyle`
+    html {
+      box-sizing: border-box;
+      height: 100%;
+    }
+
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      vertical-align: baseline;
+      height: 100vh;
+
+      ${css({
+        bg: 'background',
+        color: 'textPrimary'
+      })}
+    }
+  `
+
+  return <GlobalStyle />
+}
+
+GlobalStyles.displayName = 'GlobalStyles'
+
+const ThemeProvider = ({ children, theme }) => (
+  <StyledComponentsThemeProvider theme={theme}>
+    <>
+      <GlobalStyles />
+      {children}
+    </>
+  </StyledComponentsThemeProvider>
+)
+
+ThemeProvider.displayName = 'ThemeProvider'
+export default ThemeProvider
---
+++ src/ui/theme/colors.js
@@ -0,0 +1,8 @@
+export default {
+  colors: {
+    background: '#1D1F20',
+    surface: '#4e5255',
+    textPrimary: '#EAEBEB',
+    textSecondary: '#F3BAC3'
+  }
+}
